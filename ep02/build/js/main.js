a5aec4e7c86beae9d1aadb94f1445d987751e3ee
vipin joshi <vpnjsh55@gmail.com>
2023-09-15 20:54:00 +0530

    Basic types and initial setup

---
+++ ep02/build/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let myName = "vipin";
+let meaningOfLife;
+let isLife;
+let album;
+myName = "VJ";
+meaningOfLife = 42;
+isLife = true;
+album = true;
+//function
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(2, 4));
---
+++ ep02/src/main.ts
@@ -0,0 +1,15 @@
+let myName: string = "vipin";
+let meaningOfLife: number;
+let isLife: boolean;
+let album: any;
+
+myName = "VJ";
+meaningOfLife = 42;
+isLife = true;
+album = true;
+
+//function
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+console.log(sum(2, 4));
